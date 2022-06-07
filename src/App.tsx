3bd1de4231799b282b434b8749820b45e48224fa
Nathen-Smith <nathencsmith@gmail.com>
2022-06-07 12:24:04 -0700

    eslint fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import useDirection from './hooks/useDirection';
 
 function App() {
-  const [s, setS] = useState('HELLO')
+  const [s, setS] = useState('HELLO');
 
   const swipeRef = useDirection({
     onMoveDown: () => console.log('down'),
-    onMoveUp: function d() {console.log('up')},
-    onMoveLeft: () => console.log(s)
+    onMoveUp: function d() { console.log('up'); },
+    onMoveLeft: () => console.log(s),
   });
 
   return (
     <div className="h-screen" {...swipeRef}>
       <div className="h-32 w-32 flex rounded-xl justify-center items-center text-6xl font-bold text-white bg-gray-200">
-        hi
+        hil
       </div>
     </div>
   );
